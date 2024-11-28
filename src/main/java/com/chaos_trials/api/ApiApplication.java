2e5fb6be098e1edc763119448670984b1cf8920d
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-11-28 13:30:56 -0300

    update: atualizar main para que adicione todas variaveis do .env nas variaveis de ambiente

--- src/main/java/com/chaos_trials/api/ApiApplication.java
+++ src/main/java/com/chaos_trials/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.chaos_trials.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+		System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
+		System.setProperty("JWT_AUDIENCE", dotenv.get("JWT_AUDIENCE"));
+		System.setProperty("PORT", dotenv.get("PORT"));
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
