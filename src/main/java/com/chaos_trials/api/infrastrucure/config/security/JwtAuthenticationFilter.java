278b14cd2af3cf1a34ecc000baba743f3616d762
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-12-03 09:23:06 -0300

    feat: inclementar filter de jwt para securityfilter

---
+++ src/main/java/com/chaos_trials/api/infrastrucure/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.chaos_trials.api.infrastrucure.config.security;
+
+import com.chaos_trials.api.util.jwt.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = jwtUtil.resolveToken(request);
+
+        if (token != null && jwtUtil.validateToken(token)) {
+            String username = jwtUtil.getUsernameFromToken(request);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(username, null, null);
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // Autentica o usuário
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
