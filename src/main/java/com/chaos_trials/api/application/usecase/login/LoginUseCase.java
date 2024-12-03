88b3c6a64a4da81fb5d7e65487f7cf15c154a161
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-12-03 09:40:36 -0300

    update: atualizar return e metodos de criptografia e descriptografia

--- src/main/java/com/chaos_trials/api/application/usecase/login/LoginUseCase.java
+++ src/main/java/com/chaos_trials/api/application/usecase/login/LoginUseCase.java
@@ -1,16 +1,15 @@
 package com.chaos_trials.api.application.usecase.login;
 
+import com.chaos_trials.api.application.dto.token_key.TokenKeyDTO;
 import com.chaos_trials.api.application.form.login.LoginForm;
+import com.chaos_trials.api.application.usecase.key_generation.KeyGeneration;
 import com.chaos_trials.api.domain.model.account.Account;
 import com.chaos_trials.api.domain.repository.account.AccountRepository;
 import com.chaos_trials.api.util.jwt.JwtUtil;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -22,15 +21,17 @@ public class LoginUseCase {
 
     private final JwtUtil jwtUtil;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final KeyGeneration keyGeneration;
 
-    public LoginUseCase(AccountRepository accountRepository, JwtUtil jwtUtil) {
+    public LoginUseCase(AccountRepository accountRepository,
+                        JwtUtil jwtUtil,
+                        KeyGeneration keyGeneration) {
         this.accountRepository = accountRepository;
         this.jwtUtil = jwtUtil;
+        this.keyGeneration = keyGeneration;
     }
 
-    public ResponseEntity<String> login(LoginForm loginForm) {
+    public ResponseEntity<TokenKeyDTO> login(LoginForm loginForm) throws Exception {
         Optional<Account> optionalAccount = accountRepository.findFirstByEmailOrUsername(
                 loginForm.getUsername()
         );
@@ -39,8 +40,12 @@ public class LoginUseCase {
             if(!account.isBan()) {
                 Argon2 descriptografia = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
                 if(descriptografia.verify(account.getPassword(),loginForm.getPassword())) {
+
                     return ResponseEntity.status(HttpStatus.ACCEPTED)
-                            .body(jwtUtil.generateToken(account.getEmail()));
+                            .body(new TokenKeyDTO(
+                                    jwtUtil.generateToken(account.getEmail()),
+                                    keyGeneration.generation(account.getUuid()))
+                            );
                 }
             }
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
