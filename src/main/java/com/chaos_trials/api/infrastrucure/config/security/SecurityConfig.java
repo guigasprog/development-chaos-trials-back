b53d80cb2e34f61f01b7fb9e8852f564bee7f14b
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-12-03 09:26:43 -0300

    update: acrescentar filtro de logado ou nao para fazer requisição

--- src/main/java/com/chaos_trials/api/infrastrucure/config/security/SecurityConfig.java
+++ src/main/java/com/chaos_trials/api/infrastrucure/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.chaos_trials.api.infrastrucure.config.security;
 
+import com.chaos_trials.api.util.jwt.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,18 +9,26 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register").permitAll() // Permite login e registro
-                        .anyRequest().authenticated() // Qualquer outra requisição precisa de autenticação
-                );
+                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
