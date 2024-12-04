76d55ea71fe4dbd4b83aaf45706c6c1339bbf89b
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-12-04 13:54:35 -0300

    update: acrescentar @getter

--- src/main/java/com/chaos_trials/api/application/dto/token_key/TokenKeyDTO.java
+++ src/main/java/com/chaos_trials/api/application/dto/token_key/TokenKeyDTO.java
@@ -1,14 +1,13 @@
 package com.chaos_trials.api.application.dto.token_key;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class TokenKeyDTO {
 
-    @JsonProperty("token")
     private String token;
 
-    @JsonProperty("key")
     private String key;
 }
